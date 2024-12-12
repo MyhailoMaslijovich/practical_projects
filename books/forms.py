5a52537729e90aa8ef2d9baa65f9edd4858fdcc3
Mykhailo <157153487+Mykhailo-Maslijovich@users.noreply.github.com>
2024-12-12 20:19:22 +0200

    Delete books directory

--- books/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import book
-
-class BookForm(forms.ModelForm):
-    class Meta:
-        model = book
-        fields = ['title','author','published_data', 'isbn', 'description']
